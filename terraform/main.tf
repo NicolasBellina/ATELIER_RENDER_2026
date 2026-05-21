febd8cedaac312d53c723f12df06aeb3e7f842c9
Nicolas Bellina <nicolasbellina@MacBook-Pro-de-Nicolas.local>
2026-05-21 12:13:40 +0200

    feat: ajout de la structure React fonctionnelle

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,3 +35,15 @@ resource "render_web_service" "flask_app" {
     }
   }
 }
+
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+
+  runtime_source = {
+    image = {
+      image_url = "adminer:latest"
+    }
+  }
+}
